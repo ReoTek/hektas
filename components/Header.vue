26bfba6c857494da347e9c36fba9dff88670a40b
umutirmaksever1 <umutirmaksever@stgm.org.tr>
2023-06-07 01:16:07 +0300

    content created. header modified.

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,12 @@
 <template>
-<nav class="flex py-4 px-6 border-b-2 border-blue-900">
-  <div class="flex justify-between max-w-[800px] mx-auto w-full p-4 sm:p-6 lg:p-8">
+<nav class="flex px-6 border-b-2 border-gray-100">
+  <div class="flex justify-between max-w-[800px] mx-auto w-full p-2 sm:p-6 lg:p-8">
     <div class="flex items-center">
-      <span class="text-lg font-bold">HEKTAS</span>
+      <span class="text-lg font-bold">HEKTAŞ Tarım ve Bilim Deneyim Merkezi</span>
     </div>
     <div class="flex items-center">
       <!-- <LanguageSwitcher /> -->
-      <a class="theme-switcher" @click="toggleTheme"><span class="i-mdi-theme-light-dark text-4xl"></span></a>
+      <!-- <a class="theme-switcher" @click="toggleTheme"><span class="i-mdi-theme-light-dark text-4xl"></span></a> -->
     </div>
   </div>
 
